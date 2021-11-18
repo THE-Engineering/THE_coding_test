a720efd688651e3adec8b9d3ffc7c98244dfca5f
Andy Berry <andy.berry@timeshighereducation.com>
2021-11-18 21:19:02 +0000

    update README and other changes following a trial run of the exercise

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
-import bodyParser from "body-parser";
+import { json } from "body-parser";
+import cors from 'cors';
 
 const createApp = () => {
   const app = express();
-  app.use(bodyParser.json());
+  app.use(json());
+  app.use(cors())
 
   app.get("/", (req, res) => {
     res.send("Hello Worl!");
